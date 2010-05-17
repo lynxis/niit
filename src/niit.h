8b9381ecd753d1f17dd7a66c9202c6386b7b2963
Alexander Couzens <lynxis@c-base.org>
2010-05-17 09:15:29 +0200

    removed old defines which nowhere used

--- src/niit.h
+++ src/niit.h
@@ -4,12 +4,6 @@
  *
  */
 
-
-#define BUFF_SIZE 4096
-#define FRAG_BUFF_SIZE 1232     /* IPv6 max fragment size without IPv6 header
-                                 * to fragmanet IPv4 if result IPv6 packet will be > 1280
-                                 */
-
 #define NIIT_V6PREFIX_1 0x00000000
 #define NIIT_V6PREFIX_2 0x00000000
 #define NIIT_V6PREFIX_3 0x0000ffff
